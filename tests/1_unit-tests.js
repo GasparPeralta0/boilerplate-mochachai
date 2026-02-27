90fcc5e54f4e264c9484466cf5f8c43991e9e929
Gaspi <gaspar_peralta09@hotmail.com>
2026-02-27 16:11:21 +0100

    complete challenge #5 equal/notEqual

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -35,8 +35,8 @@ test('#isTrue, #isNotTrue', function () {
   // -----------------------------------------------------------------------------
 
   suite('Equality', function () {
-    // #5
-    test('#equal, #notEqual', function () {
+  // #5
+test('#equal, #notEqual', function () {
   assert.equal(12, '12', 'Numbers are coerced into strings with ==');
   assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
   assert.equal(6 * '2', '12');

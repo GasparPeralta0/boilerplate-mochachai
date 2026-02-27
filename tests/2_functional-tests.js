185df270d707e00e4488610cbd920fe6208c9ce1
Gaspi <gaspar_peralta09@hotmail.com>
2026-02-28 00:53:52 +0100

    complete zombie form tests (#5 #6)

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -84,37 +84,31 @@ suite('Functional Tests', function () {
     suite('"Famous Italian Explorers" form', function () {
       // #5
       test('Submit the surname "Colombo" in the HTML form', function (done) {
-        assert.fail();
-        done();
-
-    // #5
-test('Submit the surname "Colombo" in the HTML form', function (done) {
-  browser.fill('surname', 'Colombo')
-    .then(() => browser.pressButton('button[type="submit"]'))
-    .then(() => {
-      browser.assert.success();
-      browser.assert.text('span#name', 'Cristoforo');
-      browser.assert.text('span#surname', 'Colombo');
-      browser.assert.elements('span#dates', 1);
-      done();
-    })
-    .catch(done);
-});
-
+        browser.fill('surname', 'Colombo')
+          .then(() => browser.pressButton('submit'))
+          .then(() => {
+            browser.assert.success();
+            browser.assert.text('span#name', 'Cristoforo');
+            browser.assert.text('span#surname', 'Colombo');
+            browser.assert.elements('span#dates', 1);
+            done();
+          })
+          .catch(done);
       });
+ 
       // #6
-test('Submit the surname "Vespucci" in the HTML form', function (done) {
-  browser.fill('surname', 'Vespucci')
-    .then(() => browser.pressButton('submit'))
-    .then(() => {
-      browser.assert.success();
-      browser.assert.text('span#name', 'Amerigo');
-      browser.assert.text('span#surname', 'Vespucci');
-      browser.assert.elements('span#dates', 1);
-      done();
-    })
-    .catch(done);
-});
+      test('Submit the surname "Vespucci" in the HTML form', function (done) {
+        browser.fill('surname', 'Vespucci')
+          .then(() => browser.pressButton('submit'))
+          .then(() => {
+            browser.assert.success();
+            browser.assert.text('span#name', 'Amerigo');
+            browser.assert.text('span#surname', 'Vespucci');
+            browser.assert.elements('span#dates', 1);
+            done();
+          })
+          .catch(done);
+      });
     });
   });
-}); 
+});

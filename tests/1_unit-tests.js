d7e856e795013e0654db3152ef863d03752412b7
Gaspi <gaspar_peralta09@hotmail.com>
2026-02-27 16:56:18 +0100

    complete challenge #17 typeOf/notTypeOf

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -152,13 +152,15 @@ test('#property, #notProperty', function () {
   assert.property(airlinePlane, 'engines', 'planes have engines');
   assert.property(airlinePlane, 'wings', 'planes have wings');
 });
+
    // #17
 test('#typeOf, #notTypeOf', function () {
   assert.typeOf(myCar, 'object', 'myCar is an object');
   assert.typeOf(myCar.model, 'string', 'model is a string');
-  assert.typeOf(myCar.wheels, 'number', 'wheels is a number');
+  assert.fail(myCar.wheels, 'number', 'wheels is a number'); // ← esta es la tercera
   assert.typeOf(airlinePlane.engines, 'array', 'engines is an array');
   assert.typeOf(myCar.wings, 'undefined', 'wings is undefined');
+
 });
     // #18
     test('#instanceOf, #notInstanceOf', function () {
